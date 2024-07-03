69d593692a2750fb9f2ffd1108bd4ddc0a9befe6
gc-developer <morvarid7777@gmail.com>
2024-07-03 09:15:10 -0600

    Modified scraping APIs, added retriever_tool

--- models/auth.py
+++ models/auth.py
@@ -6,15 +6,6 @@ class Role(str, Enum):
     Admin = "Admin"
     User = "User"
     
-    # @classmethod
-    # def from_str(cls, value: str):
-    #     value_lower = value.lower()
-    #     if value_lower == "admin":
-    #         return cls.Admin
-    #     elif value_lower == "user":
-    #         return cls.User
-    #     else:
-    #         raise ValueError(f"Invalid role: {value}")
 
 class Signup(BaseModel):
     userName: str
